5daac756f9e2a5b31e7869e33b497be9f59a1af4
lucasandrade <lucas.andrade@roihunter.com>
2023-08-03 17:51:39 +0200

    Create documentation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,21 @@
 import express from 'express'
 import { router } from './routes'
+import swaggerUI from 'swagger-ui-express'
+import { readFileSync } from 'fs'
+import { parse } from 'yaml'
+
+const file = readFileSync(
+	`${__dirname}/documentation/documentation.yaml`,
+	'utf8'
+)
+const swaggerDocumentation = parse(file)
 
 const app = express()
 
 app.use(express.json())
 
 app.use(router)
+
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocumentation))
 
 export { app }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,5 +3,7 @@ import { app } from './app'
 const port = process.env.BACKEND_PORT
 
 app.listen(port, () => {
-	console.log(`Server is running on port: ${port}`)
+	console.log(
+		`Server is running on port: ${port}. You can access the documentation by visiting this route: http://localhost:${port}/api/docs`
+	)
 })
