fe8607776137466ec849b00bab44c58e47654b50
lucasandrade <lucas.andrade@roihunter.com>
2023-08-04 12:05:15 +0200

    Fix lint

--- backend/src/enum/EErrorMessage.ts
+++ backend/src/enum/EErrorMessage.ts
@@ -1,4 +1,4 @@
 export enum EErrorMessage {
-  KNOWN_ERROR = "An error happened with the request",
-  UNKNOWN_ERROR = "An unexpected error happened",
+	KNOWN_ERROR = 'An error happened with the request',
+	UNKNOWN_ERROR = 'An unexpected error happened'
 }
